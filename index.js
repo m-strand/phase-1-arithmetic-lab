f1d6240327d531d208a51ea7fe241c6aeb711c69
Brooke Strand <brooke.strand007@gmail.com>
2022-01-12 22:16:36 -0500

    commit tree 0dfec5ac5a708f484a2f139fd04f3f73c0c2cc6f

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 9;
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()*100 + 1);
+let mod = num3 % num4;
+let max = Math.max(1,2, 3, 5, 20);
